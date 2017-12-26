f6fd067239e3b0d395667e00f839e768618fe5e9
Mansi Agarwal <mansi.agarwal1410@gmail.com>
2017-12-25 22:36:18 -0800

    added alternate method and used set instead of dict for extra buffer

--- CHAPTER2/7_Intersecting_nodes.py
+++ CHAPTER2/7_Intersecting_nodes.py
@@ -21,10 +21,10 @@ def is_intersecting(ll1, ll2):
 def is_intersecting_method2(ll1, ll2):
 
     ptr1 = ll1.head
-    l1 = {}
+    l1 = set()
 
     while ptr1 is not None:
-        l1[ptr1] = True
+        l1.add(ptr1)
         ptr1 = ptr1.next
 
     ptr2 = ll2.head
@@ -36,6 +36,37 @@ def is_intersecting_method2(ll1, ll2):
 
     return False
 
+
+#O(n) in place
+def is_intersecting_method3(ll1, ll2):
+
+    len1 = ll1.size()
+    len2 = ll2.size()
+    diff = abs(len1 - len2)
+
+    if len1 > len2:
+        first = ll1
+        second = ll2
+    else:
+        first = ll2
+        second = ll1
+
+    # Just moving the bigger ll by difference steps , so that in the next step both lists can move together
+    ptr1 = first.head
+
+    for i in range(diff):
+        ptr1 = ptr1.next
+
+    # Now both lists can be moved together
+    ptr2 = second.head
+
+    while ptr2:
+        if ptr2 == ptr1:
+            return ptr2
+        ptr2 = ptr2.next
+        ptr1 = ptr1.next
+
+    return False
 
 
 def main():
@@ -54,15 +85,17 @@ def main():
     b.add_node_to_tail(Node(1))
     b.add_node_to_tail(Node(2))
 
-    for i in range(4,len(n)):
+    for i in range(1,len(n)):
         b.add_node_to_tail(n[i])
 
     for x in [3, 5, 6, 0, 9, 5, 3]:
         c.add_node_to_tail(Node(x))
 
-    print(is_intersecting_method2(a, b))
     print(is_intersecting(a, b))
     print(is_intersecting(a, c))
+    print(is_intersecting_method2(a, b))
+    print(is_intersecting_method3(a, b))
+
 
 
 
